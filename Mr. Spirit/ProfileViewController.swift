62cadf864c9f93355256f1a3691f0c902532f363
Ethan Perez <ethan@ethanperez.com>
2016-04-20 00:39:08 -0500

    Changed to URL based donation

--- Mr. Spirit/ProfileViewController.swift
+++ Mr. Spirit/ProfileViewController.swift
@@ -11,8 +11,7 @@ import Firebase
 import Braintree
 import SwiftyJSON
 
-class ProfileViewController: UIViewController, BTDropInViewControllerDelegate {
-    var braintreeClient: BTAPIClient?
+class ProfileViewController: UIViewController {
 
     @IBOutlet weak var orgName: UILabel!
     @IBOutlet weak var bioHeader: UILabel!
@@ -44,31 +43,30 @@ class ProfileViewController: UIViewController, BTDropInViewControllerDelegate {
         
         getCandidateInfo()
 
-        // Braintree set-up
-        let clientTokenURL = NSURL(string: "https://gatnaofft8.execute-api.us-east-1.amazonaws.com/v1/token")!
-        let clientTokenRequest = NSMutableURLRequest(URL: clientTokenURL)
-        clientTokenRequest.setValue("application/json", forHTTPHeaderField: "Accept")
-        
-        NSURLSession.sharedSession().dataTaskWithRequest(clientTokenRequest) { (data, response, error) -> Void in
-            print(error)
-            // Handle reiturned JSON
-            var clientToken:String?
-            getData: do {
-                if (data == nil){
-                    break getData
-                }
-                let jsonData = try NSJSONSerialization.JSONObjectWithData(data!, options: [])
-                let jsonDataDict = jsonData as? [String:String]
-                clientToken = jsonDataDict!["client_token"]
-            } catch let error as NSError {
-                print(error)
-            }
-            
-            if clientToken != nil {
-                self.braintreeClient = BTAPIClient(authorization: clientToken!)
-            }
-        }.resume()
-        
+//        // Braintree set-up
+//        let clientTokenURL = NSURL(string: "https://gatnaofft8.execute-api.us-east-1.amazonaws.com/v1/token")!
+//        let clientTokenRequest = NSMutableURLRequest(URL: clientTokenURL)
+//        clientTokenRequest.setValue("application/json", forHTTPHeaderField: "Accept")
+//        
+//        NSURLSession.sharedSession().dataTaskWithRequest(clientTokenRequest) { (data, response, error) -> Void in
+//            print(error)
+//            // Handle reiturned JSON
+//            var clientToken:String?
+//            getData: do {
+//                if (data == nil){
+//                    break getData
+//                }
+//                let jsonData = try NSJSONSerialization.JSONObjectWithData(data!, options: [])
+//                let jsonDataDict = jsonData as? [String:String]
+//                clientToken = jsonDataDict!["client_token"]
+//            } catch let error as NSError {
+//                print(error)
+//            }
+//            
+//            if clientToken != nil {
+//                self.braintreeClient = BTAPIClient(authorization: clientToken!)
+//            }
+//        }.resume()
     }
     
     func getCandidateInfo(){
@@ -139,17 +137,10 @@ class ProfileViewController: UIViewController, BTDropInViewControllerDelegate {
     }
     
     @IBAction func donateButtonClicked(sender: AnyObject) {
-        if (braintreeClient != nil){
-            tappedMyPayButton()
-        }
-        else {
-            let alert_title = "Oops!"
-            let alert_message = "Connection failed. Please try again."
-            
-            let alert = UIAlertController(title: alert_title, message: alert_message, preferredStyle: .Alert)
-            let action = UIAlertAction(title: "OK", style: .Default) { _ in }
-            alert.addAction(action)
-        }
+        let place = "https://mrspirit.site/donate/?name=\(candidate!.getFirstName().lowercaseString)"
+        print(place)
+        let url = NSURL(string: place)
+        UIApplication.sharedApplication().openURL(url!)
     }
     
     func getVoteCount(){
@@ -163,42 +154,6 @@ class ProfileViewController: UIViewController, BTDropInViewControllerDelegate {
         })
     }
     
-    
-    
-    func dropInViewController(viewController: BTDropInViewController, didSucceedWithTokenization paymentMethod: BTPaymentMethodNonce) {
-        // send payment
-        postNonceToServer(paymentMethod.nonce)
-        dismissViewControllerAnimated(true, completion: nil)
-    }
-    
-    func dropInViewControllerDidCancel(viewController: BTDropInViewController) {
-        dismissViewControllerAnimated(true, completion: nil)
-    }
-    
-    func tappedMyPayButton(){
-        let dropInViewController = BTDropInViewController(APIClient: braintreeClient!)
-        dropInViewController.delegate = self
-        
-        // This is where you might want to customize your view controller (see below)
-        let paymentRequest = BTPaymentRequest()
-        paymentRequest.summaryTitle = "Support \(self.candidate!.name)"
-        paymentRequest.summaryDescription = "Benefiting Make-A-Wish and Saint Louise House"
-        paymentRequest.displayAmount = "$3"
-        paymentRequest.callToActionText = "Donate"
-        
-        dropInViewController.paymentRequest = paymentRequest
-        
-        // The way you present your BTDropInViewController instance is up to you.
-        // In this example, we wrap it in a new, modally-presented navigation controller:
-        dropInViewController.navigationItem.leftBarButtonItem = UIBarButtonItem(
-            barButtonSystemItem: UIBarButtonSystemItem.Cancel,
-            target: self, action: #selector(ProfileViewController.userDidCancelPayment))
-        dropInViewController.navigationItem.leftBarButtonItem?.tintColor = UIColor(hexString: "FFFFFF")
-        let navigationController = UINavigationController(rootViewController: dropInViewController)
-        presentViewController(navigationController, animated: true, completion: nil)
-    }
-    
-    
     func getAmountRaised() {
         ref = ref.childByAppendingPath(candidate!.name)
         ref.observeSingleEventOfType(.Value, withBlock: { snapshot in
@@ -208,62 +163,6 @@ class ProfileViewController: UIViewController, BTDropInViewControllerDelegate {
                 print(error.description)
         })
     }
-    
-    func userDidCancelPayment(){
-        dismissViewControllerAnimated(true, completion: nil)
-    }
-    
-    func postNonceToServer(paymentMethodNonce: String) {
-        let paymentURL = NSURL(string: "https://gatnaofft8.execute-api.us-east-1.amazonaws.com/v1/checkout")
-        let request = NSMutableURLRequest(URL: paymentURL!)
-        let params = ["nonce": paymentMethodNonce, "amount":"3.00"] as Dictionary<String, String>
-        request.HTTPMethod = "POST"
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.addValue("application/json", forHTTPHeaderField: "Accept")
-        
-        do {
-            let jsonData = try NSJSONSerialization.dataWithJSONObject(params, options: NSJSONWritingOptions.PrettyPrinted)
-            request.HTTPBody = jsonData
-        } catch let error as NSError {
-            print(error)
-        }
-        
-        NSURLSession.sharedSession().dataTaskWithRequest(request) { (data, response, error) -> Void in
-            dispatch_async(dispatch_get_main_queue(), {
-                let json = JSON(data: data!)
-                print(json["success"].stringValue)
-                
-                var alert_title = "Oops!"
-                var alert_message = "There was an issue processing your donation. Please try again"
-                
-                if json["success"].stringValue == "true" {
-                    alert_title = "Thank You!"
-                    alert_message = "Your donation was successfully processed."
-                    let amountRaisedref = Firebase(url: "httvar//mrspirit2016.firebaseio.com/candidates/\(self.candidate!.name)/amountRaised")
-                    amountRaisedref.runTransactionBlock({
-                        (currentData:FMutableData!) in
-                        var value = currentData.value as? Double
-                        if (value == nil) {
-                            value = 0
-                        }
-                        currentData.value = value! + 3
-                        print(currentData)
-                        print(currentData.value)
-                        return FTransactionResult.successWithValue(currentData)
-                    })
-                    
-                }
-                
-                let alert = UIAlertController(title: alert_title, message: alert_message, preferredStyle: .Alert)
-                let action = UIAlertAction(title: "OK", style: .Default) { _ in }
-                alert.addAction(action)
-                
-                let rootVC = UIApplication.sharedApplication().keyWindow?.rootViewController
-                rootVC?.presentViewController(alert, animated: true){}
-            })
-        }.resume()
-    }
-
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
