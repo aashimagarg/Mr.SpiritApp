b49445a661dabb4aa3757db6e9cf5d42dfbb3f4c
vduraphe <duraphe.vaidehi@gmail.com>
2017-10-27 18:30:41 -0500

    updated profiles and twitter hashtag

--- Mr. Spirit/CandidatesViewController.swift
+++ Mr. Spirit/CandidatesViewController.swift
@@ -11,102 +11,102 @@ import Foundation
 import FirebaseDatabase
 
 class CandidatesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-  
-  @IBOutlet weak var tableView: UITableView!
-  let cellId:String = "CandidateCell"
-  
-  // Load database
-  //    var ref = Firebase(url:"httvar//mrspirit2016.firebaseio.com/candidates")
-  var ref: DatabaseReference?
-  
-  var candidatesList = [Candidate]()
-  var candidateNames = ["Alec Garcia", "Andy Wallace", "Erik Solorzano", "Jonathan Stevenson", "Marc Castaneda", "Patrick Golden", "Payne Keinarth", "Ryan Howell", "Steven Aviles"]
-  let candidateYear = ["Junior", "Junior", "Junior", "Senior", "Freshman", "Freshman", "Junior", "Sophomore", "Senior", "Sophomore", "Senior"]
-  var candidateOrgs = ["Alpha Sigma Pi", "Delta Sigma Pi", "Texas THON", "Beta Upsilon Chi", "Texas 4000", "Texas Blazers", "Texas Iron Spikes", "Camp Texas","Camp Kesem"]
-  let bioText = [ "My name is Alec \"rockhard\" because when I listen to rock, I go hard! I'm a junior biology major. I like Pina coladas, getting caught in the rain. I'm not into yoga.", "Andy is a third year Marketing and RTF dual-major from Colleyville, TX. He is very passionate about the filmography of Paul Newman, the music of Ralph Stanley, puns, and America's National Parks. He once tried to take a selfie with Mark Cuban, but Mark Cuban slapped the phone away from his face.", "On a venn diagram comparing Beyoncé and Erik Solorzano, \"Hails from H-town\" would be only one of the MANY things they have in common. As the cultured and worldly young man he is, Erik is finishing his senior year as an international relations major. He enjoys short walks on the beach, is always down for a late night visit to Pluckers, and on his most generous days is willing to share his plate of holy Mac and cheese.", "Jon Stevenson is a first year Mathematics major from Houston, Texas. In his free time, he enjoys being an over zealous dancer in various situations including but not limited to: baptisms, funerals, and even right now on stage. In the future, he'd like to put his otherwise useless math degree to use by being a teacher.", "Hello everyone, my name is Marc Castañeda and I am a third year Health Promotions major with a specialization in health fitness! I am a fun loving talkative person who determines  a friendship based on food. I love getting to know people so never be afraid to spark a conversation with me.", "My name is Patrick Thomas Golden, and I do not like spaghetti. High School Musical 1 had a great storyline and an awesome soundtrack, High School Musical 2 had a terrible storyline but a great soundtrack, and High School Musical 3 is not worth anyone's time. My favorite color is blue, and the egg came before the chicken. Hook em and I love you mom.", "Payne Keinarth is a Senior from Bastrop, TX and will be receiving his B.A. in History this May. His interests include music, sports, and spending time with friends. Starting this fall, Payne will study law at the University of Texas Law School.", "Hello. My name is Ryan Howell. My spirit animal is a meerkat. I'm glad that we are able to share this moment together and hope you have a wonderful day.", "Hi! My names Steven, I'm a Senior studying Mechanical Engineering. My likes include Buzzfeed videos, GOT, basketball, Taco Joint, and Barbarella nights before cover. My dislikes are Erik Solorzano."]
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    ref = Database.database().reference()
-    // Load candidate data
-    loadCandidates()
     
-    // Do any additional setup after loading the view.
-    tableView.delegate = self
-    tableView.dataSource = self
+    @IBOutlet weak var tableView: UITableView!
+    let cellId:String = "CandidateCell"
     
-    self.view.addSubview(self.tableView)
-  }
-  
-  func loadCandidates(){
-    var candidate = Candidate()
-    var candidateDict:AnyObject
-    var votes:Int = 0
-    var amountRaised:Double = 0
-    guard let ref = ref else {
-      return
+    // Load database
+    //    var ref = Firebase(url:"httvar//mrspirit2016.firebaseio.com/candidates")
+    var ref: DatabaseReference?
+    
+    var candidatesList = [Candidate]()
+    var candidateNames = ["Arya Behroon", "Ben Hsoulek", "Evan Welsh", "Joe Mason", "Johnathon Chang", "Miguel Martinez", "Mostefa Sheikhi", "Nick Spalding", "Thomas Le", "Uttam Eamani", "Zachary Kornblau", "Zachary Long"]
+    let candidateYear = ["Senior", "Junior", "Junior", "Sophomore", "Junior", "Junior??", "Junior", "Senior", "Senior", "Junior", "Senior", "Senior"]
+    var candidateOrgs = ["Camp Kesem", "One Note Stand, BHP", "Absolute Texxas", "Longhorn Hellraisers", "Texas THON", "Texas Blazers", "Texas Wranglers", "Texas 4000","Sailing Club", "Camp Texas", "Longhorn Singers", "Communication Council"]
+    let bioText = [ "Oh, hello there. I'm Arya Behroon, and I just want to let you know that I think I did it again. I made you believe, we're more than just friends. Oh, baby. It might seem like a crush, but it doesn't mean... that I'm serious. 'Cause to lose all my senses, that is just so typically me. Oh baby baby. What I'm trying to say is, OOPS!... I did it again. Also, that I love sloths and identify with Olaf from Frozen.", "Twerkmaster and rightful husband of Shawn Mendes, Ben Hlousek is also a triple major in Business Honors, Finance, and Health and Society. When he isn't busy carrying out Financial Director duties for One Note Stand, trying to figure out why the U.S. healthcare system is more tragic than Katy Perry's comeback, or constantly attempting to riff like Tori Kelly, Ben loves to fangirl over Ru Paul's Drag Race and sporadically drive to Waco, Dallas, and Houston to avoid school and responsibilities for no apparent reason. He hopes you enjoy and look out for his sexy dance moves, modeling, vogueing, and singing tonight!", "Hey there! My name is Evan Welsh and I am an advertising major here at UT in the Texas Media program. I am a passionate member of the spirit group Absolute Texxas, a 2018 Texas 4000 for Cancer rider, and a Camp Texas alumnus. I intern at Fons PR, an entertainment PR company here in Austin where I get to do cool things like work with studios, attend film screenings, and promote movies around Austin. I love movies and the Oscars more than anything else (I joke that the Oscars are my favorite holiday but if I'm being honest, it's not a joke) and dream of winning one someday, though I'm not sure what I'd do to get one... I also enjoy going on hikes and being outside, hanging out with friends, exploring Austin, and traveling. I can't wait to participate in Mr. Spirit so come out to the show!", "Suh dudes! It’s me, not your average Joe. When I’m not putting out fires or eating at Kismet you can catch me writing sea shanties, cracking a cold Dr. Pepper with the boys, or raising some hell. They never made an Avatar The Last Airbender movie and I have no idea what you're talking about. Also the Map is hands down the best character in Dora the Explorer.", "Hi! My name is Johnathon Chang and I'm a junior studying biomedical engineering. At any given point in time, cash me eating my weight in food (getting thicc) or crying (because school)~HOW BOUT DAT?!? Hobbies include sleeping and dabbing vigorously. Beyonce, breakfast tacos, and a good laugh/smile are the way to my heart. Ily times a milly and hook'em for life!", "Hey, how’s it going? I’m Miguel and here’s a little haiku about me:\nOne room. Three siblings\nNow six more siblings. One dream.\nTen room house. One each.\nAlso if you’re ever down for a game of Mario Party hit me up because I’m so down for a 50 turn game...as long as I’m Yoshi", "Heyoooooo my name is Mostefa and here are a bunch of random facts about me! I have a cat named kitty that I love dearly. I come from the land of the purple sprite. I tend to think about really embarrassing things I did even when I was, like, seven over and over in my head until I visibly cringe. I aspire to paint but my pre-med schedule won't even allow me to eat a proper meal. I plan to simultaneously become a doctor and the next Kanye West in the future. Quote me on that!! Lastly, I love my family and I apologize for everything you're about to see tonight (if you were even able to come through). Also follow me on soundcloud!! Also shout out to the homie Dakota!!", "My life goals have recently switched over from aspiring to see UT have just one positive football season before I leave the 40 acres, to owning my very own commemorative speedway brick. I have a knack for finding the albino squirrel and still not getting an A. I am telling you now I can out eat you anytime, anyplace, especially if it's Cabo Bob's. I swim better than I run, or walk tbh; I'm still not sure if it's from all the early swim practices or all the concussions... \n\n\"10/10 Smile\" - Robonaut, 2016 \n\"I'm gonna call you tall Intern\" - Astronaut Jack D. Fischer, 2016 \n\"Are you free to FaceTime right now?\" - Bruce Weber, 11:57PM, 09/23/2017", "It's ya boy Thomas! I'm a free spirited guy with a love for the sun, cider, and singing in the shower. You can catch me on Lake Travis or biking around the boardwalk when I'm not cooking up an omelet at my co-op. Also, I'm always down to dance- so if you have a catchy ringtone (n'sync - bye bye bye) be prepared to witness me poppin' some moves.", "Hi, my name is Uttam Eamani. My claim to fame is that I've spent well over $1000 dollars at Pluckers, have successfully brought over 100 people to 888 (the best AND only pan-asian, Vietnamese-focused restaurant in Austin), have injured my ankle over 10 times in my lifetime, and have only 1 wish: that you support me throughout my journey to become Mr. Spurtz (that's how you spell it right?).", "A senior Advertising and Economics major, Zachary Kornblau loves world peace, originality, and long walks along the Speedway bricks. When he's not in rehearsal or spending time with his Longhorn Singers friends, he spends the other 4 hours of his week doing homework or sleeping. Love you Mom and Dad!", "Yes, hello. I’m Zach Long and I’m a student at Le Cordon Bleu in Austin and I want to be the next Chopped champion. I’m a Mary Berry enthusiast and have gone as the Pioneer Woman for Halloween twice. Also, my father is Guy Fieri. So, let’s take a trip to Flavortown, y’all. Bam!"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        ref = Database.database().reference()
+        // Load candidate data
+        loadCandidates()
+        
+        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        self.view.addSubview(self.tableView)
     }
     
-    for index in 0...8 {
-      let detailPic = UIImage(named: candidateNames[index])!
-      let pic = UIImage(named: "sq-\(candidateNames[index])")!
-      
-      // Get number of current votes
-      votes = candidate.getVoteCount(candidateNames[index], ref: ref)
-      amountRaised = candidate.getAmountRaised(candidateNames[index], ref: ref)
-      
-      // Create candidate object
-      candidate = Candidate(name: candidateNames[index], organization:candidateOrgs[index], bio:bioText[index], votes: votes, amountRaised:amountRaised, headshot: pic, detailPhoto: detailPic, year: candidateYear[index])
-      
-      // Add to candidate list
-      candidatesList+=[candidate]
-      
-      // Save candidate ref
-      candidateDict = candidate.toDict()
-      candidate.saveRef(candidate.name, dict: candidateDict, ref: ref)
+    func loadCandidates(){
+        var candidate = Candidate()
+        var candidateDict:AnyObject
+        var votes:Int = 0
+        var amountRaised:Double = 0
+        guard let ref = ref else {
+            return
+        }
+        
+        for index in 0...11 {
+            let detailPic = UIImage(named: candidateNames[index])!
+            let pic = UIImage(named: "sq-\(candidateNames[index])")!
+            
+            // Get number of current votes
+            votes = candidate.getVoteCount(candidateNames[index], ref: ref)
+            amountRaised = candidate.getAmountRaised(candidateNames[index], ref: ref)
+            
+            // Create candidate object
+            candidate = Candidate(name: candidateNames[index], organization:candidateOrgs[index], bio:bioText[index], votes: votes, amountRaised:amountRaised, headshot: pic, detailPhoto: detailPic, year: candidateYear[index])
+            
+            // Add to candidate list
+            candidatesList+=[candidate]
+            
+            // Save candidate ref
+            candidateDict = candidate.toDict()
+            candidate.saveRef(candidate.name, dict: candidateDict, ref: ref)
+        }
     }
-  }
-  
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-  
-  
-  func numberOfSections(in tableView: UITableView) -> Int {
-    return 1
-  }
-  
-  
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return candidatesList.count
     
-  }
-  
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: self.cellId, for: indexPath) as! CandidateCell
-    let candidate = candidatesList[indexPath.row]
-    
-    // Configure cell
-    cell.nameLabel.text = candidate.name
-    cell.orgLabel.text = candidate.organization
-    cell.headshotImage.image = candidate.headshot
-    
-    return cell
-  }
-  
-  // MARK: - Navigation
-  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-    if segue.identifier == "profileSegue" {
-      let indexPath:IndexPath? = self.tableView!.indexPathForSelectedRow
-      
-      // Get the destination view controller
-      let detailVC:ProfileViewController = segue.destination as! ProfileViewController
-      
-      self.tableView.deselectRow(at: indexPath!, animated: false)
-      // Pass in the selected object to the new view controller
-      let candidate:Candidate = candidatesList[indexPath!.row]
-      detailVC.candidate = candidate
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
-  }
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return candidatesList.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: self.cellId, for: indexPath) as! CandidateCell
+        let candidate = candidatesList[indexPath.row]
+        
+        // Configure cell
+        cell.nameLabel.text = candidate.name
+        cell.orgLabel.text = candidate.organization
+        cell.headshotImage.image = candidate.headshot
+        
+        return cell
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "profileSegue" {
+            let indexPath:IndexPath? = self.tableView!.indexPathForSelectedRow
+            
+            // Get the destination view controller
+            let detailVC:ProfileViewController = segue.destination as! ProfileViewController
+            
+            self.tableView.deselectRow(at: indexPath!, animated: false)
+            // Pass in the selected object to the new view controller
+            let candidate:Candidate = candidatesList[indexPath!.row]
+            detailVC.candidate = candidate
+        }
+    }
 }
--- Mr. Spirit/TwitterClient.swift
+++ Mr. Spirit/TwitterClient.swift
@@ -15,47 +15,47 @@ let twitterBaseURL = URL(string: "https://api.twitter.com")
 
 
 class TwitterClient: BDBOAuth1SessionManager {
-
-  class var sharedInstance: TwitterClient {
-    struct Static {
-      static let instance = TwitterClient(baseURL: twitterBaseURL, consumerKey: twitterConsumerKey, consumerSecret: twitterConsumerSecret)
+    
+    class var sharedInstance: TwitterClient {
+        struct Static {
+            static let instance = TwitterClient(baseURL: twitterBaseURL, consumerKey: twitterConsumerKey, consumerSecret: twitterConsumerSecret)
+        }
+        return Static.instance!
     }
-    return Static.instance!
-  }
-
-  func trendingTimeline(_ success: @escaping ([Tweet]) -> (), failure: @escaping (Error) -> ()) {
-
-    let params = ["result_type" : "recent" , "count" : "100"]
-
-    //      get("1.1/search/tweets.json?f=tweets&q=%23MrSpirit2016&src=typd", parameters: params, progress: nil, success: { (task: URLSessionDataTask, response: Any?) -> Void in
-    // Changed URL so we can actually see some tweets
-    get("1.1/search/tweets.json?f=tweets&q=%23pug&src=typd", parameters: params, progress: nil, success: { (task: URLSessionDataTask, response: Any?) -> Void in
-
-      //setting initial index for dictionary
-      guard let response = response as? [String: Any] else {
-        return
-      }
-      let dictionaries = response["statuses"] as! [NSDictionary]
-
-      //print information from each tweet
-      //            for dict in dictionaries {
-      //                print("\(dict["user"]!["name"]!)")
-      //                print("\(dict["user"]!["screen_name"]!)")
-      //                print("\(dict["user"]!["profile_image_url_https"]!)")
-      //                print("\(dict["text"]!)")
-      //                print("\(dict["retweet_count"]!)")
-      //                print("\(dict["favorite_count"]!)")
-      //
-      //            }
-      let tweets = Tweet.tweetsWithArray(dictionaries)
-
-      success(tweets)
-    }, failure: { (task: URLSessionDataTask?, error: Error) -> Void in
-      failure(error)
-    })
-
-  }
-
-
-
+    
+    func trendingTimeline(_ success: @escaping ([Tweet]) -> (), failure: @escaping (Error) -> ()) {
+        
+        let params = ["result_type" : "recent" , "count" : "100"]
+        
+        //      get("1.1/search/tweets.json?f=tweets&q=%23MrSpirit2016&src=typd", parameters: params, progress: nil, success: { (task: URLSessionDataTask, response: Any?) -> Void in
+        // Changed URL so we can actually see some tweets
+        get("1.1/search/tweets.json?f=tweets&q=%23MrSpirit2017&src=typd", parameters: params, progress: nil, success: { (task: URLSessionDataTask, response: Any?) -> Void in
+            
+            //setting initial index for dictionary
+            guard let response = response as? [String: Any] else {
+                return
+            }
+            let dictionaries = response["statuses"] as! [NSDictionary]
+            
+            //print information from each tweet
+            //            for dict in dictionaries {
+            //                print("\(dict["user"]!["name"]!)")
+            //                print("\(dict["user"]!["screen_name"]!)")
+            //                print("\(dict["user"]!["profile_image_url_https"]!)")
+            //                print("\(dict["text"]!)")
+            //                print("\(dict["retweet_count"]!)")
+            //                print("\(dict["favorite_count"]!)")
+            //
+            //            }
+            let tweets = Tweet.tweetsWithArray(dictionaries)
+            
+            success(tweets)
+            }, failure: { (task: URLSessionDataTask?, error: Error) -> Void in
+                failure(error)
+        })
+        
+    }
+    
+    
+    
 }
