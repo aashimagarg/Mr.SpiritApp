7c3df3e3bc9b350f4bed8e827d561642e3a1a1c0
Aashima Garg <agarg@utexas.edu>
2017-10-28 20:07:04 -0500

    final version for 2017

--- Mr. Spirit/AboutUsViewController.swift
+++ Mr. Spirit/AboutUsViewController.swift
@@ -13,7 +13,8 @@ class AboutUsViewController: UIViewController {
   @IBOutlet weak var urlText: UIButton!
   @IBOutlet weak var aboutUsImage: UIImageView!
   @IBOutlet weak var pageantText: UILabel!
-  @IBOutlet weak var haydenKorsmo: UIImageView!
+    @IBOutlet weak var daisyHolland: UIImageView!
+    @IBOutlet weak var haydenKorsmo: UIImageView!
   @IBOutlet weak var mariaBenson: UIImageView!
   @IBOutlet weak var buyTicketsLabel: UILabel!
   @IBOutlet weak var philanthropyText: UILabel!
@@ -31,6 +32,7 @@ class AboutUsViewController: UIViewController {
     aboutUsImage.image = UIImage(named: "group")
     haydenKorsmo.image = UIImage(named: "host1")
     mariaBenson.image = UIImage(named: "host2")
+    daisyHolland.image = UIImage(named: "Daisy Holland")
     
     let aboutText = "Texas Spirits is an honorary spirit, service, and social organization at the University of Texas at Austin. The Mr. Spirit Pageant Show began three years ago as an initiative to raise money for Texas Spirits’ two philanthropies: Make-A-Wish Foundation and Saint Louise House."
     let philText = "The Make-A-Wish Foundation grants wishes to children with life-threatening medical conditions. Since its founding in 1980, the Make-A-Wish foundation has granted over 180,000 wishes and currently grants a wish every 40 minutes.\n\nSaint Louise House provides housing and services to homeless women and children in Austin, in an effort to help families stay together and encourage self-sufficiency. Saint Louise House has housed 130 families since 2012."
